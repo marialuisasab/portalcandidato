0b0a5f26f77a5bd9aa660d8cc32f394156bd87e9
Maria Luisa <marialuisasab@gmail.com>
2020-06-11 11:48:40 -0300

    reCAPTCHA v2 Google

--- resources/views/auth/footernavbartemplate.blade.php
+++ resources/views/auth/footernavbartemplate.blade.php
@@ -1,31 +1,28 @@
-
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
-
-  <title>{{ config('app.name', 'Laravel') }}</title>
-
+  <title>{{ config('app.name', 'Portal do Candidato') }}</title>
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
-
+  <!-- ReCAPTCHA -->
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-
-
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">  
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body class="idBODY">
   <div id="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-white" id="navbarLogin">
-       
+
         <!-- <a class="navbar-brand" href="{{ url('/') }}">
           <img src="img/ImagemBioextratus.png" width="160" height="50" alt="">
         </a> -->
@@ -35,199 +32,142 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <!-- Left Side Of Navbar -->
-          <ul class="navbar-nav mr-auto">
-
-          </ul>
+          <ul class="navbar-nav mr-auto"></ul>
 
           <!-- Right Side Of Navbar -->
           <ul class="navbar-nav ml-auto">
             <!-- Authentication Links -->
-            @guest
-            {{-- <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li> --}}
-
+            @guest           
             <ul>
-
               <a class="btn btn-primary mt-2" href="/">
-                <span class="fas fa-home"></span></a>
-
-              </ul>
-              @if (Route::has('register'))
+                <span class="fas fa-home"></span>
+              </a>
+            </ul>
+            @if (Route::has('register'))
               <ul class ="ml-4">
-
                 <a class="btn btn-primary mt-2" href="{{ route('register') }}">
-                  <span class="fas fa-user-plus px-2">
-                  </span></a>
-                </ul>
+                  <span class="fas fa-user-plus px-2"></span>
+                </a>
+              </ul>
+              <ul style="margin-left: auto;">
+                <div class="btn-group ml-4 mt--5" role="group" aria-label="Button group with nested dropdown" style ="width: -40px;">
+                  <div class="btn-group" role="group">
+                    <button id="btnGroupDrop1" type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <img src="/img/ImagemBrasil.png" width="40" height="30" class="d-inline-block align-top" alt="">
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                      <a class="dropdown-item" href="">
+                        <img src="/img/ImagemBrasil.png" role ="button" width="40" height="30" class="d-inline-block align-top" alt="">
+                      </a>
 
-                <ul style="margin-left: auto;">
-                  <div class="btn-group ml-4 mt--5" role="group" aria-label="Button group with nested dropdown" style ="width: -40px;">
-                    <div class="btn-group" role="group">
-                      <button id="btnGroupDrop1" type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="/img/ImagemBrasil.png" width="40" height="30" class="d-inline-block align-top" alt="">
-                      </button>
-
-                      <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-
-
-                        <a class="dropdown-item" href="">
-                          <img src="/img/ImagemBrasil.png" role ="button" width="40" height="30" class="d-inline-block align-top" alt=""></a>
-
-                          <a class="dropdown-item" href="">
-                            <img src="/img/ImagemEspanha.jpg" role ="button" width="40" height="30" class="d-inline-block align-top" alt=""></a>
-
-                            <a class="dropdown-item" href="">
-                              <img src="/img/ImagemIngles.jpg" role ="button" width="40" height="30" class="d-inline-block align-top" alt=""></a>
-
-
-                            </div>
-                          </div>
-                        </div>
-                      </ul>
-
-
-                      @endif
-                      @else
-                      <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                          {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item" href="{{ route('logout') }}"
-                          onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-                          {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                          @csrf
-                        </form>
-                      </div>
-                    </li>
-                    @endguest
-                  </ul>
+                      <a class="dropdown-item" href="">
+                        <img src="/img/ImagemEspanha.jpg" role ="button" width="40" height="30" class="d-inline-block align-top" alt="">
+                      </a>
+                      <a class="dropdown-item" href="">
+                        <img src="/img/ImagemIngles.jpg" role ="button" width="40" height="30" class="d-inline-block align-top" alt="">
+                      </a>
+                    </div>
+                  </div>
                 </div>
-             
-            </nav>
+              </ul>
 
-<div class="container-fluid" id="dadosbiocont" style="text-align: center; margin-right:10px;">
-  <div class="row justify-content-md-center" id ="FormLogin">
-    <div class="card" id="CabecalhoBody">
-      
-      <div class="container" id="containerPortalBio">
-        <div class ="row" id ="rowportal">
-         
-      
-          
-          <a class ="ExtratusLogin"href="/">Portal do Candidato <b class="BioLogin">BIO</b>EXTRATUS</a>
-          
-        </div>
-
-
-      </div>  
-      
-
-      
-              @yield('footerconteudo')
-              
-       
-
-       </div> 
-</div>
-</div>
-
-          </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<footer class="site-footer mt-5" id="idfooter">
-
-  <div class="container">
-
-    <div class="row" >
-      <div class="col-xs-4 col-md-4"></div>
-      <div class ="col-sm-4 col-md-4">
-        <h6 id="idlogininform">Para mais informações:</h6>
-      </div>
-      <div class="col-sm-4 col-md-4"></div>
-    </div>
-    <div class="row" style="margin-top: 10px; ">
-      <div class="col-sm-2 col-md-2">
-
-      </div>
-
-      <div class="col-xs-4 col-md-4">
-
-        <ul class="footer-links">
-          <li ><p class="fas fa-map-marked-alt">  Rodovia Km1, MG-123 - Zona Rural</p></li>
-          <li><p class="fas fa-phone"> (031) 3855-3000</p></li>
-          <li><p class="fas fa-envelope"> rh@bioextratus.com.br </p></li>
-        </ul>
-      </div>
-
-      <div class="col-xs-4 col-md-4">
-
-        <ul class="footer-links">
-          <li><p class="fas fa-road"> Alvinópolis - MG, CEP: 35950-000</p></li>
-          <li><p class="fas fa-registered"> CNPJ: 02.176.615/0001-07</p></li>
-        </div>
-        <div class="col-xs-2 col-md-2 ">
-
-
-        </ul>
-      </div>
-    </div>
-
-  </div>
-  <hr>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-sm-6 col-xs-12">
-        <p class="copyright-text">Copyright &copy; 2020 Todos os direitos são reservados à Bioextratus Cosmetics Natural
-          <a href="https:https://loja.bioextratus.com.br">Site Oficial</a>.
-        </p>
-      </div>
-
-      <div class="col-md-4 col-sm-6 col-xs-12">
-
-        <ul class="social-icons">
-          <li><a class="facebook" href="https://www.facebook.com/bioextratus/">
-            <img  class ="redesimg" src="/img/ImagemFacebook.jpg"></a></li>
-            <li><a class="twitter" href="https://www.linkedin.com/in/bio-extratus-cosmetic-natural-ltda-918900159/">{{--<i class="fa fa-linkedin"></i> --}}
-              <img class="redesimg" src="/img/ImagemLinkedin.jpg"></a></li>
-              <li><a class="dribbble" href="https://www.instagram.com/bioextratus/?hl=pt-br">
-                <img class ="redesimg"src="/img/ImagemInstagram.jpg" ></a></li>
-                <li><a class="youtube" href="https://www.youtube.com/bioextratusoficial">
-                  <img class="redesimg"src="/img/ImagemYoutube.png"></a></li>
-                  <li><a class="flickr" href="https://www.flickr.com/photos/bioextratus/">
-                    <img class="redesimg"src="/img/ImagemFlicker.png"></a></li>
-                  </ul>
+            @endif
+            @else
+              <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                  </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                  </form>
                 </div>
+              </li>
+            @endguest
+         </ul>
+        </div>
+      </nav>
+
+      <div class="container-fluid" id="dadosbiocont" style="text-align: center; margin-right:10px;">
+        <div class="row justify-content-md-center" id ="FormLogin">
+          <div class="card" id="CabecalhoBody">
+            <div class="container" id="containerPortalBio">
+              <div class ="row" id ="rowportal">
+                <a class ="ExtratusLogin"href="/">Portal do Candidato <b class="BioLogin">BIO</b>EXTRATUS</a>
               </div>
+            </div>  
+            @yield('footerconteudo')
+          </div> 
+        </div>
+      </div>
+
+
+
+      <footer class="site-footer mt-5" id="idfooter">
+        <div class="container">
+          <div class="row" >
+            <div class="col-xs-4 col-md-4"></div>
+            <div class ="col-sm-4 col-md-4">
+              <h6 id="idlogininform">Para mais informações:</h6>
             </div>
-           
-          </footer> 
-
-      
+            <div class="col-sm-4 col-md-4"></div>
+          </div>
+        <div class="row" style="margin-top: 10px; ">
+          <div class="col-sm-2 col-md-2"></div>
+          <div class="col-xs-4 col-md-4">
+            <ul class="footer-links">
+              <li ><p class="fas fa-map-marked-alt">  Rodovia Km1, MG-123 - Zona Rural</p></li>
+              <li><p class="fas fa-phone"> (031) 3855-3000</p></li>
+              <li><p class="fas fa-envelope"> rh@bioextratus.com.br </p></li>
+            </ul>
+          </div>
+          <div class="col-xs-4 col-md-4">
+            <ul class="footer-links">
+              <li><p class="fas fa-road"> Alvinópolis - MG, CEP: 35950-000</p></li>
+              <li><p class="fas fa-registered"> CNPJ: 02.176.615/0001-07</p></li>
+            </ul>
+          </div>
+          <div class="col-xs-2 col-md-2 "></div>
+        </div>
+      </div>
+      <hr>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-sm-6 col-xs-12">
+            <p class="copyright-text">Copyright &copy; 2020 Todos os direitos são reservados à Bioextratus Cosmetics Natural
+              <a href="https:https://loja.bioextratus.com.br">Site Oficial</a>.
+            </p>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <ul class="social-icons">
+              <li><a class="facebook" href="https://www.facebook.com/bioextratus/">
+                <img  class ="redesimg" src="/img/ImagemFacebook.jpg">
+              </a></li>
+              <li><a class="twitter" href="https://www.linkedin.com/in/bio-extratus-cosmetic-natural-ltda-918900159/">
+                <img class="redesimg" src="/img/ImagemLinkedin.jpg">
+              </a></li>
+              <li><a class="dribbble" href="https://www.instagram.com/bioextratus/?hl=pt-br">
+                <img class ="redesimg"src="/img/ImagemInstagram.jpg" >
+              </a></li>
+              <li><a class="youtube" href="https://www.youtube.com/bioextratusoficial">
+                <img class="redesimg"src="/img/ImagemYoutube.png">
+              </a></li>
+              <li><a class="flickr" href="https://www.flickr.com/photos/bioextratus/">
+                <img class="redesimg"src="/img/ImagemFlicker.png">
+              </a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer> 
 </body>
-        </html>
+</html>
 
-          
+
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,130 +2,91 @@
 <link rel="stylesheet" href="css/principal.css">
 <link rel="stylesheet" href="css/LoginCSS.css">
 <link rel="stylesheet" href="css/redes.css">
+
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
 @section('footerconteudo')
-{{-- <div class="container" id ="idcontainer">
-  <div class="row justify-content-md-center" id="FormLogin">
-    <div class="col-md-8">
-      <div class="card" id="CabecalhoBody">
-
-        <div class="container" id= "containerPortalBio">
-          <div class ="row" id ="rowportal">
-             <div class="col">
-            <p class="portallogin">Portal do Candidato</p>
-          </div>
-            
-          </div>
-          <div class="row" id ="rowbioextratus">
-             <div class="col">
-            <a class ="ExtratusLogin"href="/"><b class="BioLogin">BIO</b>EXTRATUS</a>
-             </div>
-          </div>
-
-
-        </div>  <p></p>
-        <div class="card-header"  id="HeaderID">
-          <img src="img/ImagemUsuario.png" alt="" width="40" height="40" style="justify-content: center" id ="ImagemUsuario"><h3 class="font-italic"style="font-size: 15px;">Meu Registro</h3></div>
-
-          <div class="card-body" id ="cardlogin"> --}}
-            
-            <div class="card-header"  id="HeaderID">
-        <span class="fas fa-user-plus" style="font-size: 30px; width: 40px; height: 40px;"></span><h3 class="font-italic"style="font-size: 15px;">Novo usuário</h3>
-      </div>
-            <div class="card-body" id ="cardlogin">
-            <form method="POST" action="{{ route('register') }}">
-              @csrf
-
-              <div class="input-group mb-3">
-                <label for="name">
-                </label>
-
-                  <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Nome">
-                  
-                  
-                  <div class="input-group-append">
-                     <span class="input-group-text" id="basic-addon2"><span class="fas fa-address-card" id="NomeID"></span></span>
-                    </div>
-
-
-                  @error('name')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                  @enderror
-                </div>
+  <div class="card-header"  id="HeaderID">
+    <span class="fas fa-user-plus" style="font-size: 30px; width: 40px; height: 40px;"></span><h3 class="font-italic"style="font-size: 15px;">Novo usuário</h3>
+  </div>
+  <div class="card-body" id ="cardlogin">
+    <form method="POST" action="{{ route('register') }}"  enctype="multipart/form-data" >
+    @csrf
+      <div class="input-group mb-3">
+        <label for="name"></label>
+        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Nome">
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2">
+          <span class="fas fa-address-card" id="NomeID"></span></span>
+        </div>
+        @error('name')
+        <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>          
              
-              {{-- <div class="form-group row">
-                <label for="tipo" class="col-md-4 col-form-label text-md-right">{{ __('Tipo de usuario') }}</label>
+      <div class="input-group mb-3" id ="emaillogin">
+        <label for="email"></label>
+        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2">
+            <span class="fas fa-envelope-square" id="TamEnvelopeReg"></span>
+          </span>
+        </div>
+        @error('email')
+        <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>
 
-                <div class="col-md-6">
-                  <input id="tipo" type="integer" class="form-control @error('tipo') is-invalid @enderror" name="tipo" value="{{ old('tipo') }}" required autocomplete="tipo" autofocus>
+      <div class="input-group mb-3" id="senhalogin">
+        <label for="password"></label>
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Senha">
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2"> 
+            <span class="fas fa-key" id="TamChave"></span>
+          </span>
+        </div>        
+        @error('password')
+        <span class="invalid-feedback" role="alert">
+          <strong>{{ $message }}</strong>
+        </span>
+        @enderror
+      </div>
+        
 
-                  @error('name')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                  @enderror
-                </div>
-              </div> --}}
-
-
-
-                  
-              <div class="input-group mb-3" id ="emaillogin">
-                <label for="email"></label>
-
-                  <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
-                  
-                  <div class="input-group-append">
-                     <span class="input-group-text" id="basic-addon2"><span class="fas fa-envelope-square" id="TamEnvelopeReg"></span></span>
-                    </div>
-
-                  @error('email')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                  @enderror
-                </div>
+      <div class="input-group mb-3">
+        <label for="password-confirm"></label>       
+        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password"placeholder="Confirmar Senha">
+        <div class="input-group-append">
+          <span class="input-group-text" id="basic-addon2">
+            <span class="fas fa-check-double" id="checksenha"></span> 
+          </span>
+        </div>
+      </div>
               
-              <div class="input-group mb-3" id="senhalogin">
-                <label for="password"></label>
 
-                  <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Senha">
-                    <div class="input-group-append">
-                     <span class="input-group-text" id="basic-addon2"> <span class="fas fa-key" id="TamChave"></span></span>
-                    </div>
-                    
-                    
-                  @error('password')
-                  <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                  </span>
-                  @enderror
-                </div>
-                
+      @if(env('CAPTCHA_SITE_KEY'))
+        <div class="form-group" style="text-align: center">         
+          <div class="g-recaptcha" name = "g-recaptcha-response" data-sitekey="{{ env('CAPTCHA_SITE_KEY') }}"> 
+          </div>
+          @if ($errors->has('g-recaptcha-response')) 
+              <span class="invalid-feedback" style="display: block"> 
+                  <strong>{{ $errors->first('g-recaptcha-response') }}</strong> 
+              </span> 
+          @endif
+        </div>
+      @endif
+                            
+      <div class="form-group" style="text-align: center;">        
+        <button type="submit" class="btn btn-primary" id="registrarbotao">
+          {{ __('Registrar') }}<span class="fas fa-user-check" id="userregistro"></span>
+        </button>
+      </div>
+     
+    </form>
+  </div>
 
-              <div class="input-group mb-3">
-                <label for="password-confirm"></label>
-
-               
-                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password"placeholder="Confirmar Senha">
-              <div class="input-group-append">
-                     <span class="input-group-text" id="basic-addon2"><span class="fas fa-check-double" id="checksenha"></span> </span>
-                    </div>
-              </div>
-
-              <div class="form-group" style="text-align: center;">
-                
-                  <button type="submit" class="btn btn-primary" id="registrarbotao">
-                    {{ __('Registrar') }}<span class="fas fa-user-check" id="userregistro"></span>
-                  </button>
-                </div>
-              </div>
-            </form>
-            </div>
-          
-
-
-
-    
-              @endsection
+ @endsection
